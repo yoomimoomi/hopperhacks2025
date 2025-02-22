662986a84474cbfb10ad17e095540177c934d6cd
Ash <150209573+yoomimoomi@users.noreply.github.com>
2025-02-22 14:14:50 -0500

    Revert "index reroute etc"
    This reverts commit 09091320d7dcb879efd7a7348cf5a35d2e179d8a.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react'
 import CameraFeed from './components/CameraFeed'
 import EmotionDisplay from './components/EmotionDisplay'
-import Navigation from './components/Navigation'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navigation />
       <h1>Emotion Detection</h1>
+      <CameraFeed />
+      <EmotionDisplay />
     </>
   )
 }
