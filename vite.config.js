b7cd81ee75b449d0d424032a97b46dd2926a0711
Ash <150209573+yoomimoomi@users.noreply.github.com>
2025-02-22 16:22:53 -0500

    test stuff

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
+  plugins: [react()
     ],
-  server: {
-    port: 3001, // Optional: Set a custom port
-  },
-  base: "/", // Ensure correct routing
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    }
 });
 
